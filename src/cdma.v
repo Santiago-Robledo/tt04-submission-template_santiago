dd889de7b31c682f1f753690a08f5475559ba0e0
Santiago Robledo <robledoacostasantiago@gmail.com>
2023-09-07 16:50:36 -0600

    proyecto

---
+++ src/cdma.v
@@ -0,0 +1,56 @@
+module cdma (
+    input clk_i,
+    input set_i,
+    input signal_i,
+    input [3:0] Seed_i, //4 Inputs
+    input receptor_i,   // Will be set as input.
+    output CDMA_o,
+    output Gold_o,
+    output receptor_o,
+    output reg LED_o
+    ); 
+//8 inputs and 4 outputs
+//Internal N bits wide register
+    reg [4:0] data1;
+    reg [4:0] data2;
+//Suport Variables
+    wire Gold;
+    wire CDMA;
+    reg C_Buff;
+//First Register Data  
+    always @(posedge clk_i or negedge set_i) begin
+        if (!set_i) begin
+            data1[4:0] <={Seed_i,Seed_i[0]}; //Loads the seed
+        end else begin
+            //The output is data1[4], the input is data1[0], [1 2 3 4 5] or [0 1 2 3 4]
+            data1 [4:0] <= {data1[3:0], (data1[4] ^ data1[3] ^ data1[2] ^ data1[1])}; 
+        end
+    end
+//Second register data
+    always @(posedge clk_i or negedge set_i) begin
+        if (!set_i) begin
+            data2 [4:0] <= {Seed_i,Seed_i[0]}; //Loads the seed
+        end else begin
+            //The output is data2[4], the input is data2[0], [1 2 3 4 5] or [0 1 2 3 4]
+            data2 [4:0] <= {data2[3:0], (data2[4] ^ data2[1])};
+        end
+    end
+//LED indicator
+    always @(*) begin
+       if (!Seed_i) begin
+            LED_o = 0;
+            C_Buff = 0;
+       end else begin
+            LED_o = 1;
+            C_Buff = 1;
+       end
+    end
+//Suport assignments
+assign Gold = data1[4] ^ data2[4]; //Valid support
+assign CDMA = signal_i ^ Gold;
+//Primtive Buffer
+    bufif1 B1 (CDMA_o,CDMA,C_Buff);
+//Outputs assignment
+    assign Gold_o = Gold;
+    assign receptor_o = receptor_i ^ Gold;
+endmodule
---
+++ src/tt_um_santiago.v
@@ -0,0 +1,35 @@
+`default_nettype none
+
+module tt_um_santiago #( parameter MAX_COUNT = 24'd10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // Configurar bidireccional como salidas
+  assign uio_oe = 8'b11111111;
+  
+  // Mandar todos los leds a cero
+  assign uo_out[7:0] = 8'b00000000;
+  
+  //Mandar bits restantes a cero
+  assign uio_out[7:4] = 4'b0000;
+  
+  cdma mod_cdma ( 
+	.clk_i(clk),
+    .set_i(rst_n),
+    .signal_i(ui_in[0]),
+    .Seed_i(ui_in[4:1]),
+    .receptor_i(ui_in[5]),
+    .CDMA_o(uio_out[0]),
+    .Gold_o(uio_out[1]),
+    .receptor_o(uio_out[2]),
+    .LED_o(uio_out[3])
+  );
+  
+endmodule
