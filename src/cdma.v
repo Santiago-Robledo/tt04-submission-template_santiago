aa8fcd34d00c23e6338f4ba80a509ee6027ffaee
Santiago Robledo <robledoacostasantiago@gmail.com>
2023-09-07 17:50:15 -0600

    upload3

--- src/cdma.v
+++ src/cdma.v
@@ -15,22 +15,22 @@ module cdma (
  
   always @(posedge clk_i, negedge set_i) begin
     if (~set_i) begin
-      data1[4:0] <= {seed_i, seed_i[0]};
+      data1 <= {seed_i, seed_i[0]};
     end else begin
-      data1[4:0] <= {data1[3:0], (data1[4] ^ data1[3] ^ data1[2] ^ data1[1]) }; 
+      data1 <= {data1[3:0], (data1[4] ^ data1[3] ^ data1[2] ^ data1[1]) }; 
     end
   end
 
   always @(posedge clk_i, negedge set_i) begin
     if (~set_i) begin
-      data2[4:0] <= {seed_i, seed_i[0]}; 
+      data2 <= {seed_i, seed_i[0]}; 
     end else begin
-      data2[4:0] <= {data2[3:0], (data2[4] ^ data2[1]) };
+      data2 <= {data2[3:0], (data2[4] ^ data2[1]) };
     end
   end
 
   assign led_o  = (seed_i) ? 1'b0 : 1'b1;
-	assign cdma_o = signal_i ^ gold_o;
+  assign cdma_o = signal_i ^ gold_o;
   assign gold_o = data1[4] ^ data2[4];
   assign receptor_o = receptor_i ^ gold_o;
   
